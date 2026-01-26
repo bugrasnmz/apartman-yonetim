cc1b6785669e162fb31a401a52d8a2276e03c50e
bugrasnmz <bugrasnmz@gmail.com>
2026-01-26 20:39:54 +0300

    🔑 Firebase API key düzeltildi

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,7 +3,7 @@ import { getFirestore, collection, getDocs, addDoc, updateDoc, deleteDoc, doc, s
 import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCnnnARefp_dD-e-J3WaqAHKb8EHSwg4Ws",
+    apiKey: "AIzaSyBxn6PXq7CjQGklvI8prNMIJiai2t5916w",
     authDomain: "apartman-yonetim-18730.firebaseapp.com",
     projectId: "apartman-yonetim-18730",
     storageBucket: "apartman-yonetim-18730.firebasestorage.app",
